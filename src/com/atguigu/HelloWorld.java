e558cd73fa04e7b5d2e0667da50446457d0296f4
huangbangzhu <1090809540zw@sina.com>
2016-12-11 14:44:30 +0800

    Update HelloWorld.java

--- src/com/atguigu/HelloWorld.java
+++ src/com/atguigu/HelloWorld.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class HelloWorld {
 		public static void main(String[] args) {
 			System.out.println("你好github");
+			System.out.println("你好github");
+			System.out.println("你好github");
 		}
 }
